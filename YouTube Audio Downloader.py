ff72c0db27eb9e5d20fa77342013a24422b149ef
koring <74396635+adamstore@users.noreply.github.com>
2021-10-13 09:53:17 +0100

    Add files via upload

---
+++ YouTube Audio Downloader.py
@@ -0,0 +1,28 @@
+import pafy
+
+# opening the text file which contains all the links
+file = open('C:/Users/pro/Desktop/list_youtube.txt', 'r')
+
+# loop through each link in the file
+for line in file:
+
+    # Assign link to "url" variable
+    url = line
+
+    try:
+        # Passing link to pafy
+        video = pafy.new(url)
+
+        # extracting the best available audio
+        bestaudio = video.getbestaudio()
+        print(video.title)
+
+        # downloading the extracted audio
+        bestaudio.download()
+
+    # move to next link if the video is removed in the youtube platform
+    except:
+        pass
+
+# close the text file
+file.close()
